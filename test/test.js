162651d7bb5e3e098d9ab912ea9332ec9c16b697
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2017-07-22 03:18:12 +0530

    Minor formatting change

--- test/test.js
+++ test/test.js
@@ -9,39 +9,39 @@ var collection = testData.collection, queries = testData.queries;
 
 describe('Hyper-Indexing: Enabled, Hyper-Caching: Disabled', function() {
 
-	queries.forEach(function(query) {
-		it('Search for keyword "' + query.keyword + '"', function() {
-			var config = query.config;
-			var search = searchico(collection, config);
-			var results = search.find(query.keyword);
-			chai.expect(results).to.have.same.deep.members(query.expected_result_objects);
-		});
-	});
+    queries.forEach(function(query) {
+        it('Search for keyword "' + query.keyword + '"', function() {
+            var config = query.config;
+            var search = searchico(collection, config);
+            var results = search.find(query.keyword);
+            chai.expect(results).to.have.same.deep.members(query.expected_result_objects);
+        });
+    });
 
 });
 
 describe('Hyper-Indexing: Enabled, Hyper-Caching: Enabled', function() {
 
-	queries.forEach(function(query) {
-		it('Search for keyword "' + query.keyword + '"', function() {
-			var config = query.config; config.hyper_caching = true;
-			var search = searchico(collection, config);
-			var results = search.find(query.keyword);
-			chai.expect(results).to.have.same.deep.members(query.expected_result_objects);
-		});
-	});
+    queries.forEach(function(query) {
+        it('Search for keyword "' + query.keyword + '"', function() {
+            var config = query.config; config.hyper_caching = true;
+            var search = searchico(collection, config);
+            var results = search.find(query.keyword);
+            chai.expect(results).to.have.same.deep.members(query.expected_result_objects);
+        });
+    });
 
 });
 
 describe('Hyper-Indexing: Disabled', function() {
 
-	queries.forEach(function(query) {
-		it('Search for keyword "' + query.keyword + '"', function() {
-			var config = query.config; config.hyper_indexing = false;
-			var search = searchico(collection, config);
-			var results = search.find(query.keyword);
-			chai.expect(results).to.have.same.deep.members(query.expected_result_objects);
-		});
-	});
+    queries.forEach(function(query) {
+        it('Search for keyword "' + query.keyword + '"', function() {
+            var config = query.config; config.hyper_indexing = false;
+            var search = searchico(collection, config);
+            var results = search.find(query.keyword);
+            chai.expect(results).to.have.same.deep.members(query.expected_result_objects);
+        });
+    });
 
 });
